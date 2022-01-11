64ac44732357d408f9585bddaea9a3ce2c11c136
Rosa Regalado <rosaregalado101@gmail.com>
2022-01-10 19:15:21 -0800

    test

--- app.py
+++ app.py
@@ -7,7 +7,78 @@ app = Flask("recipeBot")
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-  return "Hello world!"
+  # get the value from SMS input & convert to lowercase
+  food_input = request.values.get('Body', '').lower()
+  # search meal by name
+  api = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + food_input
+  # search by first letter
+  # api = "https://www.themealdb.com/api/json/v1/1/search.php?f=" + food_input
+  # # search random meal
+  # api = "https://www.themealdb.com/api/json/v1/1/random.php"
+
+  # make a GET request to the api -- use json formatting
+  api_data = requests.get(api).json()
+
+  # Twilio language:
+  # create a response SMS to communicate with Twilio library: MessagingResponse
+  twilio_response = MessagingResponse()
+  media_msg = twilio_response.message()
+  text_msg = twilio_response.message()
+  
+  # print themealDB API data
+  recipe = api_data['meals'][0]
+
+  # send image of prepared recipe
+  if recipe['strMealThumb'] != '':
+    media_msg.media(recipe['strMealThumb'])
+
+  # recipe ingredients msg
+  recipe_ingredients = ""
+  recipe_ingredients += "\n --------------------------- \n"
+  # add recipe name & origin:
+  recipe_ingredients += "Follow the recipe for: \n" + recipe['strMeal'] + "\n"
+  recipe_ingredients += "\nRecipe Origin: \n" + recipe['strArea'] + "\n"
+  recipe_ingredients += "\n --------------------------- \n"
+  
+  # print measures + ingredients for recipe
+  recipe_ingredients += "\nIngredients: \n"
+
+  for index in range(1,14):
+    measureKey = "strMeasure" + str(index)
+    ingredientsKey = "strIngredient" + str(index)
+
+    measureData = recipe[measureKey]
+    ingredientsData = recipe[ingredientsKey]
+
+    recipeLine = measureData + " " + ingredientsData
+    if recipeLine != "" or recipeLine != " ":
+      recipe_ingredients += recipeLine + "\n"
+  recipe_ingredients += "\n --------------------------- \n"
+
+  # recipe instructions msg
+  recipe_instructions = ""
+  recipe_instructions += "\n --------------------------- \n"
+  # print recipe instructions
+  recipe_instructions += "\nRecipe Instructions: \n" 
+  if 'strInstructions' != " ":
+    recipe_instructions += recipe['strInstructions']
+  recipe_instructions += "\n --------------------------- \n"
+  # watch video instructions
+  recipe_instructions += "\nWatch Recipe Video: \n"
+  if 'strYoutube' != " ":
+    recipe_instructions += recipe['strYoutube']
+  recipe_instructions += "\n --------------------------- \n"
+  # recipe source link
+  recipe_instructions += "\nRecipe Source: \n"
+  if 'strSource' != " ":
+    recipe_instructions += recipe['strSource']
+  recipe_instructions += "\n --------------------------- \n"
+
+
+  # add content to msg body
+  media_msg.body(recipe_ingredients)
+  text_msg.body(recipe_instructions)
+  return str(twilio_response)
 
 # messages route
 @app.route('/message', methods=['POST', 'GET'])
@@ -37,7 +108,7 @@ def sms():
   if recipe['strMealThumb'] != '':
     media_msg.media(recipe['strMealThumb'])
 
-  # ingredients msg
+  # recipe ingredients msg
   recipe_ingredients = ""
   recipe_ingredients += "\n --------------------------- \n"
   # add recipe name & origin:
@@ -60,7 +131,7 @@ def sms():
       recipe_ingredients += recipeLine + "\n"
   recipe_ingredients += "\n --------------------------- \n"
 
-  # instructions msg
+  # recipe instructions msg
   recipe_instructions = ""
   recipe_instructions += "\n --------------------------- \n"
   # print recipe instructions
